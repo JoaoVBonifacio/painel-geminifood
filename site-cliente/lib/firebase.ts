ccc3274113c8ebcb438b2604c23de83150d15a2a
JoaoVBonifácio <jboni2014@gmail.com>
2025-09-30 11:06:07 -0300

    Update firebase.ts

--- site-cliente/lib/firebase.ts
+++ site-cliente/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzlPsmqZPp3GyTqgurFd5cu8YEyjJmiPI",
-  authDomain: "geminaiappa.firebaseapp.com",
-  projectId: "geminaiappa",
-  storageBucket: "geminaiappa.firebasestorage.app",
-  messagingSenderId: "802160009521",
-  appId: "1:802160009521:web:2bb8baab8d4a7ba1229010",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
